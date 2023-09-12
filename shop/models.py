c31794ed3eaa1d23eea008c685fca5059c05ee6f
Sanjar0601 <you@example.com>
2023-09-12 23:17:11 +0500

    first commit

---
+++ shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField(max_length=100)
+    stock = models.IntegerField(max_length=100, default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
