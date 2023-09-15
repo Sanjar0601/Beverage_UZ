fd3770422b5fb960e0d5b93f43de7b6f5cf24657
Sanjar0601 <you@example.com>
2023-09-15 06:03:13 +0500

    second commit

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -14,5 +16,6 @@ class Product(models.Model):
     stock = models.IntegerField(max_length=100, default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.name
 
